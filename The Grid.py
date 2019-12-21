7c3d7501162c7fc664868ccef885fdd8926b4b4b
NathanielChavdarov <49632679+NathanielChavdarov@users.noreply.github.com>
2019-12-21 14:01:40 +0000

    Delete The Grid.py

--- The Grid.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-btns = [
-    (lambda ctl: ctl.grid(row=r, column=c) or ctl)(
-        Button())
-    for c in (0,1,2,3,4,5,6,7,8,9,10,11) for r in (0,1,2,3,4,5,6,7,8,9,10,11)]

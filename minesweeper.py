afcde723dbe3ff9b04d183b4d6267907e59f9469
NathanielChavdarov <49632679+NathanielChavdarov@users.noreply.github.com>
2019-12-22 09:45:47 +0000

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,9 +1,27 @@
-from tkinter import *
+import tkinter
 import random
 import numpy as np
 
-GRIDWIDTH = 4
-GRIDHEIGHT = 4
+GRIDWITH=4
+GRIDHEIGHT=4
+
+root = tkinter.Tk()
+root.geometry('350x350')
+root.title("MineSweeper")
+
+f = Frame(root, bd=2, height=345, width=345, bg="white")
+f.pack()
+
+c = Canvas(root, width=300, height=300)
+c.pack()
+
+buttons = np.array([
+    [
+    Button(root, height=10, width=10, bg="green") for x in range(GRIDWIDTH)
+    ]
+    for y in range(GRIDHEIGHT)
+    ]
+    )
 
 grid = np.array([
     [
